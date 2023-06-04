4832b156384e27dd44466db4a9229e1649cef0d8
Daniel Moliari <danielmoliaribarbosa@gmail.com>
2023-06-04 11:17:09 -0300

    subindo todos os arquivos do CryptoApp, para o github

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+
+const Header = () => {
+    return (
+        <Helmet>
+            <title>Crypto App</title>
+        </Helmet>
+    )
+}
+
+export default Header
